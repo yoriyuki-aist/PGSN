2458507230bbf9478103c2181438b963dd694f94
yoriyuki <yoriyuki.yamagata@aist.go.jp>
2024-08-15 10:40:03 +0900

    test_term pass

--- lambda_term.py
+++ lambda_term.py
@@ -7,16 +7,14 @@ import meta_info as meta
 import helpers
 
 Term: TypeAlias = "Term"
-Named: TypeAlias = "Named"
-Nameless: TypeAlias = "Nameless"
 
 
 def is_named(instance, attribute, value):
-    assert value.named
+    assert value.is_named
 
 
 def is_nameless(instance, attribute, value):
-    assert not value.named
+    assert not value.is_named
 
 
 def naming_context(names: set[str]) -> list[str]:
@@ -27,37 +25,19 @@ def naming_context(names: set[str]) -> list[str]:
 class Term(ABC):
     # meta_info is always not empty
     meta_info: MetaInfo = field(default=MetaInfo(), eq=False)
-    named: bool = field()
+    is_named: bool = field(validator=helpers.not_none)
 
     @classmethod
     @abstractmethod
-    def _nameless_term(cls, *kwarg):
+    def nameless(cls, meta_info=meta.empty, *kwarg) -> Term:
         pass
-
-    @classmethod
-    def nameless_term(cls, *kwarg):
-        obj = cls._nameless_term(*kwarg)
-        obj.named = False
-        return obj
 
     @classmethod
     @abstractmethod
-    def _named_term(cls, *kwarg):
+    def named(cls, meta_info=meta.empty, *kwarg) -> Term:
         pass
-
-    @classmethod
-    def named_term(cls, *kwarg):
-        obj = cls._named_term(*kwarg)
-        obj.named = True
-        return obj
-
     @abstractmethod
     def evolve(self, *kwarg):
-        pass
-
-    @named.validator
-    @abstractmethod
-    def _named_validator(self, attribute, value):
         pass
 
     @abstractmethod
@@ -65,9 +45,9 @@ class Term(ABC):
         pass
 
     def eval_or_none(self) -> Term | None:
-        assert not self.named
+        assert not self.is_named
         evaluated = self._eval_or_none()
-        assert (evaluated is None) or (not evaluated.named)
+        assert (evaluated is None) or (not evaluated.is_named)
         return evaluated
 
     def eval(self) -> Term:
@@ -79,9 +59,9 @@ class Term(ABC):
         pass
 
     def shift(self, num: int, cutoff: int) -> Term:
-        assert not self.named
+        assert not self.is_named
         shifted = self._shift(num, cutoff)
-        assert not shifted.named
+        assert not shifted.is_named
         return shifted
 
     @abstractmethod
@@ -89,10 +69,10 @@ class Term(ABC):
         pass
 
     def subst_or_none(self, variable: int, term: Term) -> Term | None:
-        assert not self.named
-        assert not term.named
+        assert not self.is_named
+        assert not term.is_named
         substituted = self._subst_or_none(variable, term)
-        assert not substituted.named
+        assert not substituted.is_named
         return substituted
 
     def subst(self, variable:int, term: Term) -> Term:
@@ -105,59 +85,57 @@ class Term(ABC):
         pass
 
     def free_variables(self) -> set[str]:
-        assert self.named
-        return self.free_variables()
+        assert self.is_named
+        return self._free_variables()
 
     @abstractmethod
     def _remove_name_with_context(self, context: list[str]) -> Term:
         pass
 
     def remove_name_with_context(self, context: list[str]) -> Term:
-        assert self.named
+        assert self.is_named
         nameless = self._remove_name_with_context(context)
-        assert not nameless.named
+        assert not nameless.is_named
         return nameless
 
     def my_naming_context(self) -> list[str]:
-        assert self.named
+        assert self.is_named
         return naming_context(self.free_variables())
 
     def remove_name(self) -> Term:
-        assert self.named
+        assert self.is_named
         return self.remove_name_with_context(self.my_naming_context())
+
+    def __call__(self, arg: Term) -> Term:
+        assert self.is_named == arg.is_named
+        return App(self, arg, is_named=self.is_named)
 
 
 @frozen
 class Variable(Term):
     num: int | None = field()
     name: str | None = field()
-    named: bool = field()
+    is_named: bool = field()
 
     @classmethod
-    def _nameless_term(cls, num=None, meta_info=meta.empty):
-        assert num is not None
-        return Variable(num=num, name=None, meta_info=meta_info, named=False)
+    def nameless(cls, num=0, meta_info=meta.empty) -> Variable:
+        return Variable(num=num, name=None, meta_info=meta_info, is_named=False)
 
     @classmethod
-    def _named_term(cls, name=None, meta_info=meta.empty):
-        assert name is not None
-        return Variable(num=None, name=name, named=False, meta_info=meta_info)
+    def named(cls, name=name, meta_info=meta.empty) -> Variable:
+        return Variable(num=None, name=name, is_named=True, meta_info=meta_info)
+
+    @classmethod
+    def from_name(cls, name:str):
+        return cls.named(name=name)
 
     def evolve(self, num: int = None, name: str = None):
-        if num is None and name is not None and self.named:
-            assert self.num is None and self.name is not None
-            return evolve(self, name=name, named=True)
-        if num is not None and name is None and not self.name:
-            assert self.num is not None and self.name is None
-            return evolve(self, num=num, named=False)
+        if num is None and name is not None:
+            return evolve(self, num=None, name=name, is_named=True)
+        if num is not None and name is None:
+            return evolve(self, num=num, name=None, is_named=False)
         else:
             assert False
-
-    def _named_validator(self, attribute, value):
-        if value:
-            assert self.num is None and self.name is not None
-        else:
-            assert self.num is not None and self.name is None
 
     def _free_variables(self) -> set[str]:
         return {self.name}
@@ -171,7 +149,7 @@ class Variable(Term):
         else:
             return self.evolve(num=self.num+d)
 
-    def _subst_or_none(self, num, term) -> Nameless | None:
+    def _subst_or_none(self, num, term) -> Term | None:
         if self.num == num:
             return term
         else:
@@ -184,35 +162,33 @@ class Variable(Term):
 
 @frozen
 class Abs(Term):
-    v: Variable | None = field(validator=helpers.not_none)
+    v: Variable | None = field()
     t: Term = field(validator=helpers.not_none)
 
-    def _named_validator(self, attribute, value):
-        if value:
-            assert self.v is not None and self.v.named and self.t.named
-        else:
-            assert self.v is None and not self.named
+    @classmethod
+    def nameless(cls, t=t, meta_info=meta.empty):
+        assert not t.is_named
+        return Abs(is_named=False, v=None, t=t, meta_info=meta_info)
 
     @classmethod
-    def _nameless_term(cls, t, meta_info=meta.empty):
-        assert t.named is False
-        return Abs(named=False, v=None, t=t, meta_info=meta_info)
+    def named(cls, v=v, t=t):
+        assert v is not None and v.is_named and t.is_named
+        return Abs(is_named=True, v=v, t=t, meta_info=meta.empty)
 
-    @classmethod
-    def _named_term(cls, v, t):
-        assert v is not None and v.named and t.named
-        return Abs(named=True, v=v, t=t, meta_info=meta.empty)
+    def __attr_post_init__(self):
+        assert self.v.is_named == self.t.is_named
+
 
     def evolve(self, t: Term, v: Variable | None = None):
-        if v is None and not t.named:
-            return evolve(self, v=v, t=t, named=False)
-        elif v is not None and v.named and t.named:
-            return evolve(self, v=v, t=t, named=True)
+        if v is None and not t.is_named:
+            return evolve(self, v=v, t=t, is_named=False)
+        elif v is not None and v.is_named and t.is_named:
+            return evolve(self, v=v, t=t, is_named=True)
         else:
             assert False
 
     def _eval_or_none(self) -> Term | None:
-        assert not self.named
+        assert not self.is_named
         t_evaluated = self.t.eval()
         return None if t_evaluated is None else self.evolve(t=t_evaluated)
 
@@ -234,7 +210,7 @@ class Abs(Term):
     def _remove_name_with_context(self, context: list[str]) -> Term:
         new_context = [self.v.name] + context
         name_less_t = self.t.remove_name_with_context(new_context)
-        return self.evolve(t=name_less_t)
+        return self.evolve(t=name_less_t, v=None)
 
 
 @frozen
@@ -243,37 +219,33 @@ class App(Term):
     t2: Term = field(validator=helpers.not_none)
 
     @classmethod
-    def _nameless_term(cls, t1: Term, t2: Term):
-        assert not t1.named and not t2.named
-        return App(named=False, t1=t1, t2=t2)
+    def nameless(cls, t1: Term = t1, t2: Term = t2, meta_info: MetaInfo = meta.empty):
+        assert not t1.is_named and not t2.is_named
+        return App(is_named=False, t1=t1, t2=t2, meta_info=meta_info)
 
     @classmethod
-    def _named_term(cls, t1: Term, t2: Term):
-        assert t1.named and t2.named
-        return App(named=True, t1=t1, t2=t2)
+    def named(cls, t1: Term = t1, t2: Term = t2):
+        assert t1.is_named and t2.is_named
+        return App(is_named=True, t1=t1, t2=t2)
 
     @classmethod
     def term(cls, t1: Term, t2: Term):
-        if t1.named and t2.named:
-            return cls.named_term(t1, t2)
-        elif not t1.named and not t2.named:
-            return cls.nameless_term(t1, t2)
+        if t1.is_named and t2.is_named:
+            return cls.named(t1, t2)
+        elif not t1.is_named and not t2.is_named:
+            return cls.nameless(t1, t2)
         else:
             assert False
 
-    def _named_validator(self, attribute, value):
-        if self.t1.named and self.t2.named:
-            assert self.named
-        elif not self.t1.named and not self.t2.named:
-            assert not self.named
-        else:
-            assert False
+
+    def __attr_post_init__(self):
+        assert self.t1.is_named == self.t2.is_named
 
     def evolve(self, t1: Term, t2:Term):
-        if t1.named and t2.named:
-            return evolve(self, t1=t1, t2=t2, named=True)
-        elif not t1.named and not t2.named:
-            return evolve(self, t1=t1, t2=t2, named=False)
+        if t1.is_named and t2.is_named:
+            return evolve(self, t1=t1, t2=t2, is_named=True)
+        elif not t1.is_named and not t2.is_named:
+            return evolve(self, t1=t1, t2=t2, is_named=False)
         else:
             assert False
 
@@ -311,7 +283,7 @@ class App(Term):
     def _remove_name_with_context(self, context: list[str]) -> Term:
         nameless_t1 = self.t1.remove_name_with_context(context)
         nameless_t2 = self.t2.remove_name_with_context(context)
-        return self.evolve(t1=self.t1, t2=self.t2)
+        return self.evolve(t1=nameless_t1, t2=nameless_t2)
 
 
 # Constants: any python data is okay
@@ -320,18 +292,15 @@ class Constant(Term):
     name: str = field(validator=helpers.not_none)
 
     @classmethod
-    def _nameless_term(cls, name=name):
-        return Constant(name=name, named=False)
+    def named(cls, meta_info=meta.empty, name=name):
+        return Constant(name=name, is_named=True)
 
     @classmethod
-    def _named_term(cls, name=name):
-        return Constant(name=name, named=True)
+    def nameless(cls, meta_info=meta.empty, name=name):
+        return Constant(name=name, is_named=False)
 
-    def evolve(self, name: str, named: bool):
+    def evolve(self, name: str, is_named: bool):
         return evolve(self, name=name)
-
-    def _named_validator(self, attribute, value):
-        assert True
 
     def _eval_or_none(self) -> Term | None:
         return None
@@ -346,29 +315,32 @@ class Constant(Term):
         return set()
 
     def _remove_name_with_context(self, context: list[str]) -> Term:
-        return evolve(self, named=False)
+        return evolve(self, is_named=False)
 
 
 # Builtin functions.  Arity is always one.
 @frozen
 class Builtin(Term):
-    name: str
+    name: str = field(validator=helpers.not_none)
 
     @classmethod
-    def _nameless_term(cls, name):
-        return Builtin(name=name, named=False)
+    def nameless(cls, meta_info=meta.empty, name=name):
+        return cls(name=name, is_named=False, meta_info=meta_info)
 
     @classmethod
-    def _named_term(cls, name):
-        return Builtin(name=name, named=True)
+    def named(cls, meta_info=meta.empty, name=name):
+        return cls(name=name, is_named=True)
 
-    def eval_or_none(self) -> None:
+    def evolve(self, name=name):
+        return evolve(self, name=name)
+
+    def _eval_or_none(self) -> None:
         return None
 
-    def shift(self, d, cutoff) -> Builtin:
+    def _shift(self, d, cutoff) -> Builtin:
         return self
 
-    def subst_or_none(self, num, term) -> None:
+    def _subst_or_none(self, num, term) -> None:
         return None
 
     @abstractmethod
@@ -376,7 +348,7 @@ class Builtin(Term):
         pass
 
     def applicable(self, arg: Term) -> bool:
-        if self.named or arg.named:
+        if self.is_named or arg.is_named:
             return False
         else:
             return self._applicable(arg)
@@ -389,13 +361,11 @@ class Builtin(Term):
         assert self.applicable(arg)
         return self._apply_arg(arg)
 
-    def __call__(self, arg: Term) -> Term:
-        if self.applicable(arg):
-            return self.apply_arg(arg)
-        else:
-            return App.term(self, arg)
+    def _free_variables(self) -> set[str]:
+        return set()
 
-
+    def _remove_name_with_context(self, context: list[str]) -> Term:
+        return evolve(self, is_named=False)
 
 
 
--- tests/test_term.py
+++ tests/test_term.py
@@ -1,8 +1,8 @@
 import meta_info
 import lambda_term
-import string_term
-import list_term
-import record_term
+# import string_term
+# import list_term
+# import record_term
 
 
 def test_meta_info():
@@ -11,33 +11,33 @@ def test_meta_info():
 
 
 def test_var():
-    var_x = lambda_term.NamedVariable.from_name('x')
-    assert var_x.meta_info.name_info == 'x'
+    info = meta_info.MetaInfo(name_info='x')
+    var_x = lambda_term.Variable.named(name='x', meta_info=info)
     nameless_x = var_x.remove_name()
     assert nameless_x.meta_info.name_info == 'x'
 
 
 def test_lambda_term_id():
-    var_x = lambda_term.NamedVariable.from_name('x')
-    id_f = lambda_term.NamedAbs(v=var_x, t=var_x)
-    assert id_f == lambda_term.NamedAbs(v=var_x, t=var_x)
-    t = lambda_term.NamedApp(t1=id_f, t2=id_f)
-    assert t == lambda_term.NamedApp(t1=id_f, t2=id_f)
+    var_x = lambda_term.Variable.from_name('x')
+    id_f = lambda_term.Abs.named(v=var_x, t=var_x)
+    assert id_f == lambda_term.Abs.named(v=var_x, t=var_x)
+    t = lambda_term.App.named(t1=id_f, t2=id_f)
+    assert t == lambda_term.App.named(t1=id_f, t2=id_f)
     nameless_t = t.remove_name()
     reduced = nameless_t.eval()
     assert reduced == id_f.remove_name()
 
 
 def test_lambda_term_const():
-    c = lambda_term.Constant('c')
-    var_x = lambda_term.NamedVariable.from_name('x')
-    id_f = lambda_term.NamedAbs(v=var_x, t=var_x).remove_name()
-    t = lambda_term.App(t1=id_f, t2=c)
+    c = lambda_term.Constant.nameless('c')
+    var_x = lambda_term.Variable.from_name('x')
+    id_f = lambda_term.Abs.named(v=var_x, t=var_x).remove_name()
+    t = lambda_term.App.nameless(t1=id_f, t2=c)
     assert t.eval() == c
 
 
 class Id(lambda_term.Builtin):
-    def applicable(self, _):
+    def _applicable(self, _):
         return True
 
     def _apply_arg(self, arg):
@@ -45,66 +45,10 @@ class Id(lambda_term.Builtin):
 
 
 def test_builtin():
-    id_f = Id('id')
-    c = lambda_term.Constant('c')
+    id_f = Id.nameless('id')
+    c = lambda_term.Constant.nameless('c')
     assert id_f.applicable(c)
     assert id_f.apply_arg(c) == c
-    t = lambda_term.App(id_f, c)
+    t = lambda_term.App.nameless(id_f, c)
     assert t.eval() == c
-
-
-def test_string():
-    s = string_term.String('x')
-    assert s.value == 'x'
-
-
-def test_list():
-    var_x = lambda_term.NamedVariable.from_name('x')
-    id_f = lambda_term.NamedAbs(v=var_x, t=var_x)
-    t = lambda_term.NamedApp(id_f, var_x)
-    ll = list_term.NamedList([var_x, id_f, t])
-    ll1 = ll.remove_name()
-    reduced = ll1.eval()
-    assert isinstance(reduced, list_term.List)
-    assert reduced.terms[0] == var_x.remove_name()
-    assert reduced.terms[1] == id_f.remove_name()
-    assert reduced.terms[2] == var_x.remove_name()
-
-
-def test_record():
-    var_x = lambda_term.NamedVariable.from_name('x')
-    id_f = lambda_term.NamedAbs(v=var_x, t=var_x)
-    t = lambda_term.NamedApp(id_f, var_x)
-    r = record_term.NamedRecord({'x': var_x, 'id_f': id_f, 't':t})
-    reduced = r.remove_name().eval()
-    assert isinstance(reduced, record_term.Record)
-    assert reduced.terms['x'] == var_x.remove_name()
-    assert reduced.terms['id_f'] == id_f.remove_name()
-    assert reduced.terms['t'] == var_x.remove_name()
-#
-# def test_lambda_term_data():
-#     s1 = lambda_term.ConstantNamed('test')
-#     assert s1.value == 'test'
-#     s2 = s1.remove_name()
-#     assert s2.value == s1.value   # type inference error
-#
-#
-# def test_lambda_term_list():
-#     context = ['x', 'y']
-#     var_x = lambda_term.NamedVariable('x')
-#     var_y = lambda_term.NamedVariable('y')
-#     list_term_1 = lambda_term.ListNameless([var_x.remove_name_with_context(context),
-#                                             var_y.remove_name_with_context(context)])
-#     list_term_2 = lambda_term.ListNamed([var_x, var_y])
-#     assert list_term_1 == list_term_2.remove_name_with_context(context)
-#
-#
-# def test_lambda_term_record():
-#     context = ['x', 'y']
-#     var_x = lambda_term.NamedVariable('x')
-#     var_y = lambda_term.NamedVariable('y')
-#     list_term_1 = lambda_term.RecordNameless({'a': var_x.remove_name_with_context(context),
-#                                               'b': var_y.remove_name_with_context(context)})
-#     list_term_2 = lambda_term.RecordNamed({'a':var_x, 'b':var_y})
-#     assert list_term_1 == list_term_2.remove_name_with_context(context)
-
+    assert id_f(c).eval() == c
